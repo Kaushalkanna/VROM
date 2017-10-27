a9e77ff7fde29bf6b6d15c5141dfe48d4474a200
Kaushalkanna <kaushalkanna@gmail.com>
2017-10-27 13:22:28 +0530

    model cleanup

--- vasavi_rates/models.py
+++ vasavi_rates/models.py
@@ -1,16 +1,10 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
 
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
     product_rate = models.IntegerField(default=0)
-    product_pub_date = models.DateTimeField(default=timezone.now, editable=False)
+    product_pub_date = models.DateTimeField(editable=False, auto_now=True)
 
     def __str__(self):
         return '{}: {}/Q'.format(self.product_name.upper(), str(self.product_rate))
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
